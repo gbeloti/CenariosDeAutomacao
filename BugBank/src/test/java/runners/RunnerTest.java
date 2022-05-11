12835efe5485c6ea085d4a30292f34bcfe22916a
gbeloti <98908097+gbeloti@users.noreply.github.com>
2022-05-11 15:16:16 -0300

    Update RunnerTest.java

--- BugBank/src/test/java/runners/RunnerTest.java
+++ BugBank/src/test/java/runners/RunnerTest.java
@@ -11,11 +11,10 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 		plugin = {"pretty", "html:target/cucumber-report"},
 		features = "src/test/resources/features",
 		glue = "stepsDefinitions",
-		tags = "@registrar_contaSemSaldo",
+//		tags = "@registro",
 		snippets = SnippetType.CAMELCASE,
 		monochrome = true,
 		dryRun = false
-//		dryRun = true
 		)
 
 public class RunnerTest {}
